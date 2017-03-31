467ca26ed0dcb2606cc9bff794ccc6e9544444eb
Dominic Breuker <dominic.breuker@gmail.com>
2017-03-31 11:33:09 +0200

    change print

--- resnet_50/training.py
+++ resnet_50/training.py
@@ -27,7 +27,7 @@ def extract_features(extension, img_size):
         name = "{}x{}_{}".format(img_size[0], img_size[1], "feature-maps")
         model = build_resnet_50(cls_head=None)
         image_files, features = extractor.extract(name, model, img_size)
-    print(features.shape)
+    print("Features extracted. Shape: {}".format(features.shape))
     return image_files, features
 
 
